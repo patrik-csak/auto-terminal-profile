da1dc119342255f24c92062e103fc2a1eddb5e6e
Patrik Csak <p@trikcsak.com>
2026-03-14 12:30:38 -0700

    update lint dependencies

--- prettier.config.js
+++ prettier.config.js
@@ -7,7 +7,7 @@ const config = {
 	singleQuote: true,
 	useTabs: true,
 
-	// https://github.com/JoshuaKGoldberg/eslint-plugin-package-json/tree/v0.83.0#usage-alongside-prettier
+	// https://github.com/JoshuaKGoldberg/eslint-plugin-package-json/tree/v0.91.0#usage-alongside-prettier
 	plugins: ['prettier-plugin-packagejson'],
 };
 

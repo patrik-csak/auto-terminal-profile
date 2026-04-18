5604402a06d7c305a14a162445fd821e4adcc440
Patrik Csak <p@trikcsak.com>
2026-04-17 19:00:39 -0700

    remove prettier-plugin-packagejson
    it was removed from the readme in eslint-plugin-package-json v0.91.1:
    https://github.com/michaelfaith/eslint-plugin-package-json/compare/v0.91.0...v0.91.1#diff-b335630551682c19a781afebcf4d07bf978fb1f8ac04c6bf87428ed5106870f5L189-L193

--- prettier.config.js
+++ prettier.config.js
@@ -6,9 +6,6 @@ const config = {
 	bracketSpacing: false,
 	singleQuote: true,
 	useTabs: true,
-
-	// https://github.com/JoshuaKGoldberg/eslint-plugin-package-json/tree/v0.91.0#usage-alongside-prettier
-	plugins: ['prettier-plugin-packagejson'],
 };
 
 export default config;

3a703a853f6344fd19f3960d1eead5ab26447246
Patrik Csak <p@trikcsak.com>
2026-07-29 08:43:18 -0700

    improve quality checks

--- eslint.config.js
+++ eslint.config.js
@@ -1,1 +1,15 @@
-export {default} from 'xo/eslint-adapter';
+import {defineConfig, globalIgnores} from 'eslint/config';
+import xo from 'eslint-config-xo';
+
+export default defineConfig([
+	globalIgnores(['package-lock.json']),
+
+	...xo(),
+
+	{
+		rules: {
+			// TODO: Fix these
+			'node-test/prefer-context-mock': 'off',
+		},
+	},
+]);

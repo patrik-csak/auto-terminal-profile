74f2575c3919842e988ad820b1f723d70b320718
Patrik Csak <p@trikcsak.com>
2026-09-22 21:52:47 -0700

    update and simplify linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,7 @@
-import {defineConfig, globalIgnores} from 'eslint/config';
+import {defineConfig} from 'eslint/config';
 import xo from 'eslint-config-xo';
 
 export default defineConfig([
-	globalIgnores(['package-lock.json']),
-
 	...xo(),
 
 	{
